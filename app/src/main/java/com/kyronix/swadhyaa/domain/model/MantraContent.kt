a2176af4371d18d9d6eccc5c925f2bedc6051d66
ዐቢይ-ምንጭ-ፋይሎች <infinitydattaashim1210958@gmail.com>
2026-09-02 18:49:20 +0600

    Add files via upload

---
+++ app/src/main/java/com/kyronix/swadhyaa/domain/model/MantraContent.kt
@@ -0,0 +1,20 @@
+package com.kyronix.swadhyaa.domain.model
+
+/**
+ * UI-facing mantra payload from core.db.
+ */
+data class MantraContent(
+    val id: Int,
+    val vedaId: Int,
+    val vedaCode: String,
+    val vedaName: String,
+    val level1: Int?,
+    val level2: Int?,
+    val level3: Int?,
+    val mantraNo: Int?,
+    val sanskrit: String,
+    val rishi: String?,
+    val devata: String?,
+    val chhanda: String?,
+    val refLabel: String
+)
