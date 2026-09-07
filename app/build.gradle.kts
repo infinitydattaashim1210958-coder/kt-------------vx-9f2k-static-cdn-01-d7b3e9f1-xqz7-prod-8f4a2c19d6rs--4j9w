25b83e5cb11d28cf1e9fd285fbeebd15d795158f
ዐቢይ-ምንጭ-ፋይሎች <infinitydattaashim1210958@gmail.com>
2026-09-07 17:47:29 +0600

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    testImplementation("junit:junit:4.13.2")
 }
