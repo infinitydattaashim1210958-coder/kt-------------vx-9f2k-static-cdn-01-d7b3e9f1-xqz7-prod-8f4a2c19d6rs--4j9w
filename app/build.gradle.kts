86cb8c75553dfad7e061ad6c2ad9682070a3c64c
ዐቢይ-ምንጭ-ፋይሎች <infinitydattaashim1210958@gmail.com>
2026-09-07 13:29:34 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
